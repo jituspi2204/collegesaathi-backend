a648354183d1ad81e5f78b20566013f602546afd
Ankit Vashisht <ankitvashisht12@gmail.com>
2021-01-18 21:29:09 +0530

    update userCartModel.js

--- models/userCartModel.js
+++ models/userCartModel.js
@@ -3,24 +3,34 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userCartSchema = Schema({
-	title: {
-		type: String, 
-		require = true
-	},
-	image: {
-		type: String,
-		require: true
-	},
-	category: {
-		type: String,
-		require: true
-	},
-	price: {
-		type: Number,
-		require: true
-	},
 	quantity: {
 		type: Number, 
 		require: true
+	},
+	userId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User',
+		require: true
+	},
+	sellerId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Seller',
+		require: true
+	},
+	sellerCartId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'SellerCart',
+		require: true
+	},
+	createdAt: {
+		type: mongoose.Schema.Types.Date,
+		default: new Date.now()
+	},
+	updatedAt: {
+		type: mongoose.Schema.Types.Date,
+		default: new Date.now()
 	}
 });
+
+const UserCart = mongoose.model('UserCart', userCartSchema);
+module.exports = UserCart;
