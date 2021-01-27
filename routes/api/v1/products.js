4a955b5e5ef5bf98a37b37c0d219fcfa42bbd858
Jitendra Kumar Sharma <jitendra@quikieapps.com>
2021-01-27 10:19:21 +0530

    new images added

--- routes/api/v1/products.js
+++ routes/api/v1/products.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 const productController = require('../../../controllers/productController');
-
+const addProduct = require('../../../addData');
 /* GET users listing. */
 router.get('/search', productController.getProduct);
+router.get('/add-product', addProduct.addData);
 router.get('/seller-products',productController.sellerProducts);
 
 
