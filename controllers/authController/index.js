f6042c1159537dcfb761d29c6a030dd8dc30064d
Jitendra Kumar Sharma <jitendra@quikieapps.com>
2021-05-15 19:03:33 +0530

    done

--- controllers/authController/index.js
+++ controllers/authController/index.js
@@ -76,7 +76,7 @@ exports.verifyStudentMiddleware = hoc(async (req, res, next) => {
     try {
         let payload = await jwtUtils.verifyToken(token);
         if (payload) {
-            let user = await Student.findById(payload._id).populate('notifications');
+            let user = await Student.findOne(payload.email).populate('notifications');
             req.user = user;
             next();
         } else {
